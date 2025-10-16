a56a8695153c53f075127bf68bd11664b132bfa5
shokohsc <shokohsc@gmail.com>
2025-10-16 15:00:25 +0200

    chore(): added search bangs

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -32,8 +32,32 @@ onMounted(() => {
     }
 });
 
+const bangs = [
+    { bang: "!re", url: "https://www.reddit.com/search/?q=%s" },
+    { bang: "!rt", url: "https://www.rottentomatoes.com/search?search=%s" },
+	{ bang: "!npm", url:"https://www.npmjs.com/search?q=%s" },
+	{ bang: "!wr", url: "https://www.wordreference.com/enfr/%s" },
+	{ bang: "!tr", url: "https://www.deepl.com/fr/translator#en/fr/%s" },
+	{ bang: "!imdb", url: "https://www.imdb.com/find?s=all&q=%s" },
+	{ bang: "!gh", url: "https://github.com/search?q=%s" },
+	{ bang: "!yt", url: "https://www.youtube.com/results?search_query=%s" },
+	{ bang: "!d", url:  "https://www.larousse.fr/dictionnaires/francais/%s" },
+	{ bang: "!g", url:  "https://www.google.com/search?q=%s" },
+	{ bang: "!c", url:  "https://www.conjugaisonfrancaise.com/recherche.html?q=%s" },
+	{ bang: "!iad", url:"https://www.phind.com/search?q=%s&c=&source=searchbox&init=true" },
+	{ bang: "!gmap", url: "https://www.google.fr/maps?hl=fr&q=%s" },
+	{ bang: "!img", url:"https://duckduckgo.com/?q=%s&iax=images&ia=images" },
+	{ bang: "!ia", url: "https://www.perplexity.ai/search/?q=%s" },
+    { bang: "!yr", url: "https://www.yr.no/en/search?q=%s" }
+]
+
 // Run the search and go to the results page
 const onSearch = async () => {
+    const bangMatch = bangs.find(b => searchTerm.value.startsWith(b.bang))
+    if (undefined !== bangMatch) {
+        await window.open(bangMatch.url.replace('%s', encodeURIComponent(searchTerm.value.substring(bangMatch.bang.length + 1))), "_blank")
+        return
+    }
     await searchStore.fetchResults(searchTerm.value);
     router.push({ name: 'Results', query: { q: searchTerm.value } }); // push query param
 };
